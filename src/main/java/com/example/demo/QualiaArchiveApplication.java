0745c9ff0b495c9013b70d852054c23bb432941d
shigaisen <shigaisen@localhost>
2026-03-14 14:00:48 +0900

    test

---
+++ src/main/java/com/example/demo/QualiaArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualiaArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualiaArchiveApplication.class, args);
+	}
+
+}
